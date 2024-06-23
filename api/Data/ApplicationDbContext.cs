2f3a27ec77de3727aa894baa21fa77fb7cae82aa
danujav <danujagreru@gmail.com>
2024-06-24 00:20:20 +0530

    table migration done

---
+++ api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace api;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options)
+    : base(options)
+    {
+
+    }
+    public DbSet<Stock> Stocks { get; set; }
+    public DbSet<Comment> Comments { get; set; }
+}
